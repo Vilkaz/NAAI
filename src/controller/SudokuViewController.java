cf144469aae3d786a6b7b92bb3f1692e1c6c58ca
vk <vilius.kukanauskas@mydata.de>
2015-07-30 13:28:19 +0200

    initialisierung

--- src/controller/SudokuViewController.java
+++ src/controller/SudokuViewController.java
@@ -1,9 +1,11 @@
-package sample;
+package controller;
 
 import javafx.scene.Node;
 import javafx.scene.layout.VBox;
 
 import java.util.List;
+
+import static java.lang.Math.sqrt;
 
 /**
  * User: Roman Hecht
@@ -11,8 +13,9 @@ import java.util.List;
  * Date: 30.07.2015
  * Time: 12:58 PM
  */
-public class SudokuView {
+public class SudokuViewController {
     public static Node getNodeFromList(List list){
+        double lineLength = sqrt(list.size());
         return new VBox();
     }
 }
